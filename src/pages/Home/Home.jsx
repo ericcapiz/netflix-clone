81e2fc1c8b78b1ba14bd9a7fe4359a7253e33481
ericcapiz <ericcapiz@gmail.com>
2021-06-29 13:08:21 -0500

    nav bar started

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/Navbar/Navbar"
+import "./home.scss"
+
+
+export default function Home() {
+    return (
+        <div className="home">
+            <Navbar />
+        </div>
+    )
+}
