5a347ae25e0f1dcb39798454919b80fd1082bfcd
ericcapiz <ericcapiz@gmail.com>
2021-06-29 16:15:06 -0500

    login component started

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./app.scss"
+import Login from './pages/Login/Login'
+// import Register from "./pages/Register/Register";
 // import Home from "./pages/Home/Home";
-import Watch from "./pages/Watch/Watch";
+// import Watch from "./pages/Watch/Watch";
 
 
 const App = () => {
-  return <Watch/>;
+  return <Login /> ;
 };
 
 export default App;
